0ae669d9cef7448f852c8b1071a31e87874f38e5
Daalleee <dalemasan10@gmail.com>
2025-11-03 21:50:57 +0700

    tambahkan tabel  foto di model produk

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'image_path',
+    ];
+
+    /**
+     * Get the product that owns the image.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
