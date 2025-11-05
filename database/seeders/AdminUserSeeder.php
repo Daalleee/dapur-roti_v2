4c44ba7da80a44217dd214377f9cd2874d870d30
Daalleee <dalemasan10@gmail.com>
2025-11-05 08:26:50 +0700

    buat akun seeder untuk admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -13,13 +13,19 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'nama' => 'Admin User',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('password'),
-            'no_hp' => '081234567890',
-            'alamat' => 'Jl. Contoh Alamat No. 123, Kota, Provinsi',
-            'role' => 'admin',
-        ]);
+        // Create admin user if not exists
+        User::updateOrCreate(
+            [
+                'email' => 'admin@gmail.com',
+            ],
+            [
+                'nama' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'),
+                'no_hp' => '0828-9183-83',
+                'alamat' => 'Weri',
+                'role' => 'admin',
+            ]
+        );
     }
 }
